88ad88ab29fa8417e2d8786ae846cbb1a8a817af
Kartik Mehta <kartikmehta202@gmail.com>
2024-05-02 03:39:15 +0530

    Ad Alan AI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
+import { useEffect } from "react";
 import "./App.css";
-import {Layout} from './layout/Layout.jsx'
+import alanBtn from "@alan-ai/alan-sdk-web";
+import { Layout } from "./layout/Layout.jsx";
 function App() {
+  useEffect(() => {
+    alanBtn({
+      key: "02b27ce51747cefbd610608f6aad42102e956eca572e1d8b807a3e2338fdd0dc/stage",
+      onCommand: (commandData) => {
+        if (commandData.command === "go:back") {
+          // Call the client code that will react to the received command.
+        }
+      },
+    });
+  }, []);
   return (
     <>
-     <Layout/>
+      <Layout />
     </>
   );
 }
