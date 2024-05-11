cce7d4ed3e83be8a8c28f38ba29dc6c78599a5fb
Kartik Mehta <kartikmehta202@gmail.com>
2024-05-12 02:47:16 +0530

    Documentation 4/n

--- frontend/src/Context/AuthContext.jsx
+++ frontend/src/Context/AuthContext.jsx
@@ -1,4 +1,12 @@
-import { createContext, useContext, useEffect, useReducer } from "react";
+/**
+ * @description: AuthContext is used to store the user data in the local storage and provide the user data to the components.
+ */
+
+// Importing necessary hooks and components.
+import { createContext, useEffect, useReducer } from "react";
+import PropTypes from "prop-types";
+
+// Creating the initial state.
 const initialState = {
   user:
     localStorage.getItem("user") !== undefined
@@ -8,9 +16,12 @@ const initialState = {
   token: localStorage.getItem("token") || null,
 };
 
-
+// Creating the AuthContext.
 export const AuthContext = createContext(initialState);
+
+// Creating the authReducer.
 const authReducer = (state, action) => {
+  // Switch case to handle different actions.
   switch (action.type) {
     case "LOGIN_START":
       return {
@@ -38,9 +49,12 @@ const authReducer = (state, action) => {
   }
 };
 
+// Defining the AuthContextProvider.
 export const AuthContextProvider = ({ children }) => {
+  // Using the useReducer hook.
   const [state, dispatch] = useReducer(authReducer, initialState);
 
+  // Using the useEffect hook to store the user data in the local storage.
   useEffect(() => {
     localStorage.setItem("user", JSON.stringify(state.user));
     localStorage.setItem("role", state.role);
@@ -59,4 +73,8 @@ export const AuthContextProvider = ({ children }) => {
       {children}
     </AuthContext.Provider>
   );
+};
+
+AuthContextProvider.propTypes = {
+  children: PropTypes.node,
 };
